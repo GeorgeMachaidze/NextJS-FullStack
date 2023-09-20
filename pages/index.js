883a0c2a5e3ff7eec611e1b88634bfc14cdce0e9
Wakana <wakanichi13@gmail.com>
2023-09-20 17:12:50 +0400

    feat: add getStaticProps function

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
+import path from "path";
+
+import fs from "fs/promises";
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((products) => (
+        <li key={products.id}>{products.title}</li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
 
 export default HomePage;
